ee00aced588f5bd3adc767f437a763156bab213d
Martab1 <martabernardo86@gmail.com>
2021-03-19 17:04:40 +0100

    the end

--- js/script.js
+++ js/script.js
@@ -28,10 +28,8 @@ CALCOLO PREZZO BIGLIETTO DEL TRENO
 var nKilometri =  parseInt( prompt ( "quanti kilometri vuoi percorrere? "));
 // console.log("l'utente vuole percorrere: ", nKilometri , "km");
 
-
 // costante prezzo al km
 var kmPrice1 = 0.21;
-
 
 // prezzo base viaggio 
 var costo = nKilometri * kmPrice1;
@@ -54,18 +52,17 @@ var eta = parseInt( prompt ("quanti anni hai?"));
          2° se sei over  65  -40%             */
 
 
-
 if (eta < 18) {
-   var sconto = costo * ( 20 / 100);
+   var sconto = costo * ( 20 / 100) ;
 //    console.log("il prezzo per il cliente minorenne è: " ,costo - result ,"€");
-   document.getElementById("result").innerHTML = " sei minorenne! con il 20% di sconto per te il prezzo è di "+ (costo - sconto) +  "€";
+   document.getElementById("result").innerHTML = " sei minorenne! con il 20% di sconto per te il prezzo è di "+ (costo - sconto).toFixed(2) +  "€";
 }  else if ( eta >= 65) {
-   var sconto = costo * ( 40 / 100);
+   var sconto = costo * ( 40 / 100) ;
 //    console.log("il prezzo per il cliente over 65 è: " , costo - sconto, "€");
-   document.getElementById("result").innerHTML = " sei un over 65! con il 40% di sconto per te il prezzo è di "+ (costo - sconto) + "€";
+   document.getElementById("result").innerHTML = " sei un over 65! con il 40% di sconto per te il prezzo è di "+ (costo - sconto).toFixed(2) + "€";
 } else {
 //    console.log("non ci sono sconti applicati ");
-  document.getElementById("result").innerHTML = " non ci sono sconti applicati il prezzo è di " + costo + "€";
+  document.getElementById("result").innerHTML = " non ci sono sconti applicati il prezzo è di " + costo.toFixed(2) + "€";
 }
     
 
